1de7a2500609a40011f6cb5154da31181a5f3ff9
Alex Peterson <helloalexpeterson+git@gmail.com>
2013-02-04 14:09:00 -0600

    Created project, added teacher code

---
+++ U01_BelowTargetReport/U01_BelowTargetReport/U01_BelowTargetReport.cpp
@@ -0,0 +1,145 @@
+/*
+ * CIS 276 Introduction to C/C++ Programming
+ * Chuck Nelson, Professor
+ *
+ * Student Information (please complete ? below)
+ *    Name:    ?
+ *    Email:   s???????@student.rockvalleycollege.edu
+ * Class Information
+ *    Section:      ?
+ * Assignment Information
+ *    Project Name:  U01_BelowTargetReport
+ *    Source  Name:  U01_BelowTargetReport.cpp
+ *---------------------------------------------------------------------
+ */
+
+
+
+// preprocessor directives----
+#include <iostream>                    // for program input & output
+#include <string>                      // for string class data
+#include <iomanip>                     // for formatting output
+using namespace std;
+
+/* Function: main() *************************************************
+ *
+ * application entry point
+ */
+
+// STUDENT CODE BEGINS
+
+
+
+   /* declarations ***********************************************
+      declare program data
+   */
+
+
+
+
+
+
+   /* statements *************************************************
+      code program instructions
+   */
+
+   //
+   // display program headings
+   //
+
+
+
+   // enter program loop (do while)
+
+
+
+      //
+      // Step #1: input
+      //
+
+
+
+
+      //
+      // Step #2: processing
+      //
+
+
+
+
+
+      //
+      // Step #3: output
+      //
+
+
+
+      // display the report column headings
+      // format & print column heading row
+
+
+
+      // display the report detail line
+      // 1 - format & display user name
+      cout << left;                       // justification
+      cout << setw(18);                   // width for column data
+      cout << name;
+
+      // 2 - format & display target contribution
+
+
+
+
+      // 3 - format & display actual contribution
+
+
+
+
+
+      // 4 - format & display difference
+      // note: positive number means target met
+
+
+
+
+
+      // see if the user wants to continue or to exit the program
+
+
+
+
+
+
+
+
+
+   //
+   // program termination
+   //
+
+
+
+
+
+
+
+
+
+// end of main() function ----------------------------------------
+
+// STUDENT CODE ENDS
+
+
+
+//-------------------------------------------------------------------
+// General Functions
+//-------------------------------------------------------------------
+
+// <none>
+
+//-------------------------------------------------------------------
+// Utility Functions
+//-------------------------------------------------------------------
+
+// <none>
+
